b477688a89f772f714f070ad7836610f3d1522d4
KalaHanok <hanokkala123@gmail.com>
2023-11-17 12:17:59 +0530

    Student id no extraction is build

--- AutoAttendence/AutoAttendence/urls.py
+++ AutoAttendence/AutoAttendence/urls.py
@@ -1,4 +1,4 @@
-"""AutoAttendence URL Configuration
+"""StudentEntryExit URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from pro import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.getIndexPage,name="home"),
+    path('login/',views.loginView),
+    path('logout/',views.logoutView,name="logout")
 ]
